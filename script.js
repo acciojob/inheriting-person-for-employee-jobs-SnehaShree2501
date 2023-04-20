88c2735e29769b04b7710d76552ed6147af158fb
sneha shree <snehashree2501@gmail.com>
2023-04-20 21:47:35 +0530

    Inherit Person for Employee

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+   this.name=name;
+   this.age=age;
+}
+Person.prototype.greet()=function(){
+    console.log("Hello, my name is $(this.name) I am $(this.age) years old.");
+}
+function Employee(name, age, jobTitle) {
+    this.name=name;
+    this.age=age;
+    this.jobTitle=jobTitle;
+}
+//super.Person.greet();
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
 
-function Employee(name, age, jobTitle) {}
+Employee.prototype.jobGreet()=function(){
+    console.log("Hello, my name is $(this.name), I am $(this.age) years old, and my job title is $(this.jobTitle)");
+}
+
 
 // Do not change code below this line
 window.Person = Person;
